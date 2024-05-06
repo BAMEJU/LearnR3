9a2b80d7bba8b10dca98222e8d47bc74e50c4789
BAMEJU <julia.bandres@cbmed.at>
2024-05-06 12:23:15 +0200

    Some basic set up

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -4,27 +4,30 @@ usethis::use_data(mmash, overwrite = TRUE)
 
 library(here)
 
-#Download
+# Download
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
 
 # download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 
-#Unzip files
+# Remove previous `mmash/` folder to have clean update
+dir_delete(here("data-raw/mmash/"))
+
+# Unzip files
 unzip(here("data-raw/mmash-data.zip"),
-      exdir = here("data-raw"),
-      junkpaths = TRUE
+  exdir = here("data-raw"),
+  junkpaths = TRUE
 )
 
 Sys.sleep(1)
 unzip(here("data-raw/MMASH.zip"),
-      exdir = here("data-raw")
+  exdir = here("data-raw")
 )
 
-#Remove/Tidy up left over files
+# Remove/Tidy up left over files
 library(fs)
 file_delete(here(c(
-    "data-raw/MMASH.zip",
-    "data-raw/SHA256SUMS.txt",
-    "data-raw/LICENSE.txt"
+  "data-raw/MMASH.zip",
+  "data-raw/SHA256SUMS.txt",
+  "data-raw/LICENSE.txt"
 )))
 file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
