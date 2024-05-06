4d56ac048dccdfc36963ae0d1d3abde9dc35f53f
BAMEJU <julia.bandres@cbmed.at>
2024-05-06 11:41:35 +0200

    Start of project

---
+++ data-raw/mmash.R
@@ -0,0 +1,30 @@
+## code to prepare `mmash` dataset goes here
+
+usethis::use_data(mmash, overwrite = TRUE)
+
+library(here)
+
+#Download
+mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
+
+# download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
+
+#Unzip files
+unzip(here("data-raw/mmash-data.zip"),
+      exdir = here("data-raw"),
+      junkpaths = TRUE
+)
+
+Sys.sleep(1)
+unzip(here("data-raw/MMASH.zip"),
+      exdir = here("data-raw")
+)
+
+#Remove/Tidy up left over files
+library(fs)
+file_delete(here(c(
+    "data-raw/MMASH.zip",
+    "data-raw/SHA256SUMS.txt",
+    "data-raw/LICENSE.txt"
+)))
+file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
